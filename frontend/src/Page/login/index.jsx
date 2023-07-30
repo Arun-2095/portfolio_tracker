2b36fa18e62efa1cc30ecdf5023a073e52144b24
arunkumar <arunagiri2195@gmail.com>
2023-07-30 17:40:24 +0530

    text change

--- frontend/src/Page/login/index.jsx
+++ frontend/src/Page/login/index.jsx
@@ -4,7 +4,7 @@ import "./login.scss"
 export default function Login() {
   return (
     <a href="http://localhost:5000/auth" className='login-btn d-flex align-items-center justify-content-between'>
-        <p className='px-2'> Login in  with </p>
+        <p className='px-2'> Login with </p>
         <Avatar alt="Remy Sharp" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png" />
     </a>
   )
